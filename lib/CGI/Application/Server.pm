257d33d560b1a1e8dced892b75380678ba48f74c
Ricardo SIGNES <rjbs@cpan.org>
2009-02-08 21:04:59 -0500

    disable annoying noise

--- lib/CGI/Application/Server.pm
+++ lib/CGI/Application/Server.pm
@@ -77,8 +77,8 @@ sub is_valid_entry_point {
 sub handle_request {
     my ($self, $cgi) = @_;
     if (my ($path, $target) = $self->is_valid_entry_point($ENV{REQUEST_URI})) {
-        warn "$ENV{REQUEST_URI} ($target)\n";
-        warn "\t$_ => " . param( $_ ) . "\n" for param();
+        # warn "$ENV{REQUEST_URI} ($target)\n";
+        # warn "\t$_ => " . param( $_ ) . "\n" for param();
 
         local $ENV{CGI_APP_RETURN_ONLY} = 1;
         (local $ENV{PATH_INFO} = $ENV{PATH_INFO}) =~ s/\A\Q$path//;
